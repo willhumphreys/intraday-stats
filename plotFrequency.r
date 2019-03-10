eeeaca7b6fa3da9c2ee7fcdad54178b1e430e3de
Will Humphreys <whumphreys@gmail.com>
2019-03-10 17:26:02 +0100

    Format day and time better

--- plotFrequency.r
+++ plotFrequency.r
@@ -33,10 +33,14 @@ removeOutlier <- function(data, outliers) {
 dayName <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 dayLookup <- data.frame(dayName)
 
+timeName <- c("1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "0:00")
+timeLookup <- data.frame(timeName)
+
 dayOfWeek <- dayLookup$dayName[as.numeric(day)]
+hourOfDay <- timeLookup$timeName[as.numeric(hour)]
 
 print(paste('day is', day, 'day of week is', dayOfWeek))
-
+print(paste('hour is', hour, 'hour of day', hourOfDay))
 
 data.path = file.path('results', 'data', paste(input, '.csv', sep = ''))
 
@@ -51,7 +55,8 @@ dataFixed <- removeOutlier(data, outliers)
 
 ggplot(data = dataFixed, aes(x = range, y = frequency)) +
     geom_bar(stat = "identity", fill = "steelblue") +
-    ggtitle(paste('Frequency against range for ', input, hour, dayOfWeek)) +
+    ggtitle(paste('Frequency against range for ', input, hourOfDay, dayOfWeek)) +
+    scale_x_continuous(breaks = seq(0, 30, 2), limits = c(0, 30)) +
     theme_light()
 
 outputLocation <- file.path('results', 'graphs', paste(input, '-', dayOfWeek, '-', hour, '.png', sep = ''))
